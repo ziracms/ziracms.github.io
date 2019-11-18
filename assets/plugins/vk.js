a1a0df3c6eca2c80091f40d4c9341b1d651c13a9
Zira <ziracms@gmail.com>
2019-11-18 21:42:16 +0500

    vk community id changed

--- assets/plugins/vk.js
+++ assets/plugins/vk.js
@@ -2,5 +2,5 @@ window.onload = function() {
     VK.init({apiId: 7213413, onlyWidgets: true});
     VK.Widgets.Comments("vk_comments", {limit: 10, attach: "graffiti,photo"});
     VK.Widgets.Like("vk_like", {type: "button"});
-    VK.Widgets.CommunityMessages("vk_community_messages", 127607773, {disableExpandChatSound: "1",tooltipButtonText: "Есть вопрос?"});
+    VK.Widgets.CommunityMessages("vk_community_messages", 188925450, {disableExpandChatSound: "1",tooltipButtonText: "Есть вопрос?"});
 }
