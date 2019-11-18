68482219263c1a33b23fbfedfcd710368b3fa08a
Zira <ziracms@gmail.com>
2019-11-18 22:22:03 +0500

    Vk group widget added

--- assets/plugins/vk.js
+++ assets/plugins/vk.js
@@ -3,4 +3,5 @@ window.onload = function() {
     VK.Widgets.Comments("vk_comments", {limit: 10, attach: "graffiti,photo"});
     VK.Widgets.Like("vk_like", {type: "button"});
     VK.Widgets.CommunityMessages("vk_community_messages", 188925450, {disableExpandChatSound: "1",tooltipButtonText: "Есть вопрос?"});
+    VK.Widgets.Group("vk_groups", {mode: 1}, 188925450);
 }
