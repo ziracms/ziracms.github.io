92d9f9e1818686f25f9adbf2ad07ef758734dd1b
Zira <ziracms@gmail.com>
2019-11-25 20:19:15 +0500

    minor fix

--- assets/plugins/vk.js
+++ assets/plugins/vk.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    //VK.init({apiId: 7213413, onlyWidgets: true});
+    VK.init({apiId: 7213413, onlyWidgets: true});
     //VK.Widgets.Comments("vk_comments", {limit: 10, attach: "graffiti,photo"});
     VK.Widgets.Like("vk_like", {type: "button"});
     VK.Widgets.CommunityMessages("vk_community_messages", 188925450, {disableExpandChatSound: "1",tooltipButtonText: "Есть вопрос?"});
